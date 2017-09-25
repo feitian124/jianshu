957b46a06080a4be069959a0695aa43c3ac1b9b9
刘思雅 <sophia@liusiyadeMacBook-Air.local>
2017-09-25 23:17:41 +0800

    get with superagent

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
-var http = require("http");
+var superagent = require("superagent");
+var cheerio = require('cheerio');
 
-var url = "http://www.alloyteam.com/";
-var data = "";
+var url = "http://www.jianshu.com/u/309baf0877d2";
 
-// 创建一个请求
-var req = http.request(url, function(res){
-    // 设置显示编码
-    res.setEncoding("utf8");
-    // 数据是 chunked 发送，意思就是一段一段发送过来的
-    // 我们使用 data 给他们串接起来
-    res.on('data', function(chunk){
-        data += chunk;
+superagent
+    .get(url)
+    .end(function(req, res){
+        var $ = cheerio.load(res.text);
+        $('#list-container > ul > li > .content > .title').each(function(index, item){
+            var txt = $(this).text();
+            console.log(txt);
+        })
     });
-    // 响应完毕时间出发，输出 data
-    res.on('end', function(){
-        // dealData(data);
-        console.log(data);
-    });
-});
-
-// 发送请求
-req.end();
